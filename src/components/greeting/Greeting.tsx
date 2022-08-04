6c22a4b27e85c46f5af209bfd4e138f9b60d7728
Matt <matt@multicode.co.uk>
2022-08-04 18:15:54 +0000

    on click

--- src/components/greeting/Greeting.tsx
+++ src/components/greeting/Greeting.tsx
@@ -1,7 +1,18 @@
 import './Greeting.css';
 
 const Greeting = (props : GreetingProps) => {
-    return <p className="greeting-paragraph" >Hello {props.name}. You are {props.age} years old.</p>
+
+    let name =props.name;
+
+    const changeName = () => {
+        name="Jim";
+        console.log("button was clicked");
+    };
+
+    return <div>
+                <p className="greeting-paragraph" >Hello {name}. You are {props.age} years old.</p>
+                <button onClick={changeName}  >Change my name</button>
+        </div>
 };
 
 type GreetingProps = {
